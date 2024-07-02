29b4a054b4408059fead27b0069ac136fdcb35a3
tadeo <tadeoacosta14@gmail.com>
2024-07-02 01:16:13 -0300

    ultimo

--- router/authRouter.js
+++ router/authRouter.js
@@ -1,14 +1,27 @@
 const express = require("express");
 const usuarioModel = require('../models/usuario-model.js');
-const { crearUsuario } = require("../controllers/authControllers.js");
 const routerAuth = express.Router();
-
 
 routerAuth.post('/login', (req, res) => {
     res.send('login');
 });
 
-routerAuth.post('/registro', crearUsuario);
+routerAuth.post('/registro',async (req, res) => {
+    
+    const { name, edad, email, password } = req.body;
+    
+    //validaciones
+    if (name === '' || edad === '' || email === '' || password === '') {
+        res.send('Todos los campos son obligatorios');
+    }
+    
+    const usuario = new usuarioModel(req.body);
+              
+    
+    await usuario.save();
+
+    res.send('usuario creado');
+});
 
 
 
