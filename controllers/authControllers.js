a5047d50c33875bfc5c94f1912a7666cd9491366
tadeo <tadeoacosta14@gmail.com>
2024-07-13 01:04:21 -0300

    verificamos si el usuario estaba creado

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,21 +1,31 @@
 const usuarioModel = require("../models/usuario-model");
 
-const crearUsuario  =  async (req, res) => {
+const crearUsuario = async (req, res) => {
     
-    const { name, edad, email, password } = req.body;
+    try {
+        const { name, edad, email, password } = req.body;
 
     //validaciones 
     if (name === "" || edad === "" || email === "" || password === "") {
         res.send('Todos los campos son obligatorios');
-    }
+        }
+        //Analizamos si el usuario no fue registrado con email
+        let usuario = await usuarioModel.findOne({ email });
+        console.log(usuario);
 			
-    const usuario = new usuarioModel (req.body);
+     usuario = new usuarioModel(req.body);
 
     //guardamos en la base de datos
     await usuario.save();
 
 
-    res.send('usuario creado');
-}
+        res.send('usuario creado');
+        
+    } catch (error) {
+        console.log(error);
+    }
+    
+    
+};
 
 module.exports = { crearUsuario };
