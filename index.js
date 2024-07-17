03c421c8a8a5c3c1c10eadf53aafa2fb19457d19
tadeo <tadeoacosta14@gmail.com>
2024-07-17 19:46:56 -0300

    cambios

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const dbConnection = require('./database/config');
+const cors = require('cors');
 const app = express(); 
 require('dotenv').config();
 
 // Middleware para manejar las solicitudes JSON
 app.use(express.json()); 
+
+// Middleware para manejar CORS
+app.use(cors({
+	origin: 'http://localhost:5173', // Permitir solicitudes desde tu frontend
+  }));
+
 
 // Middleware para las rutas de autenticación
 app.use('/auth', require('./router/authRouter'));
