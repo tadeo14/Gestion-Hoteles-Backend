727af0bf439f5dcfbf45d7c39fbcce7791ada976
tadeo <tadeoacosta14@gmail.com>
2024-07-27 19:57:46 -0300

    comenzamos con el get, es mostrar la lista de habitaciones

--- router/adminRouter.js
+++ router/adminRouter.js
@@ -3,7 +3,8 @@ const { crearHabitacion } = require("../controllers/adminControllers");
 const routerAdmin = express.Router();
 
 //Creacion de los endpoints para gestionar las habitaciones 
-//routerAdmin.get('/habitaciones',listaHabitaciones);
+routerAdmin.get('/habitaciones', listaHabitaciones);
+
 routerAdmin.post('/crearHabitacion',crearHabitacion);
 //routerAdmin.put('/editarHabitacion',editarHabitacion);
 //routerAdmin.delete('/eliminarHabitacion',eliminarHabitacion);
