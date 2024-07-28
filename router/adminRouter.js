d32c2c1bfa216f7aff7d59110a278bdf7bc4c56b
tadeo <tadeoacosta14@gmail.com>
2024-07-28 11:56:10 -0300

    resolvemos conflictos

--- router/adminRouter.js
+++ router/adminRouter.js
@@ -5,6 +5,9 @@ const routerAdmin = express.Router();
 //Creacion de los endpoints para gestionar las habitaciones 
 routerAdmin.get('/habitaciones', listaHabitaciones);
 
+//Creacion de los endpoints para gestionar las habitaciones 
+routerAdmin.get('/habitaciones', listaHabitaciones);
+
 routerAdmin.post('/crearHabitacion',crearHabitacion);
 routerAdmin.put('/editarHabitacion',editarHabitacion);
 routerAdmin.delete('/eliminarHabitacion/:id',eliminarHabitacion);
