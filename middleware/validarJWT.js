9a4b8603b8efb39fefb8e210c2d4c16ff34bf4c5
Fabrizio <fabrigabrielcoronel@gmail.com>
2024-08-05 02:04:42 -0300

    token conectado al login y rol

--- middleware/validarJWT.js
+++ middleware/validarJWT.js
@@ -1,32 +1,32 @@
 const jwt = require('jsonwebtoken');
 
 const validarJWT = (req, res, next) => {
-  
-  try {
-    
-      //recibimos el token atravez de los header y definimos un nombre en este caso le puse x-token
-    const token = req.header('x-token');
+    try {
+        // Recibimos el token a través de los headers y definimos un nombre en este caso 'x-token'
+        const token = req.header('x-token');
 
-      //si no recibimos un token, ejemplo lo borraron del localstorage tiramos un error
-    if (!token) {
+        // Si no recibimos un token, tiramos un error
+        if (!token) {
+            return res.status(401).json({
+                msg: 'No hay token en la petición',
+            });
+        }
+
+        // Verificar el token
+        const decoded = jwt.verify(token, process.env.SECRET_JWT);
+        
+        // Asignar la información del token decodificado al objeto req
+        req.usuario = decoded;
+
+    } catch (error) {
+        // Si el token es inválido o está vencido
         return res.status(401).json({
-            msg: 'No hay token en la peticion',
+            msg: 'Token no válido o vencido',
         });
     }
-    
-    const verificarToken = jwt.verify(token, process.env.SECRET_JWT);
 
-      
-  } catch (error) {
-      //si el token es invalida cae aca, en caso de que este vencido
-      return res.status(401).json({
-          msg: 'Token vencido',
-      });
-  }  
-    //Next deja ejecutar el siguiente middleware y si no hay mas ejecuta la funcion del flujo  
+    // Next deja ejecutar el siguiente middleware y si no hay más, ejecuta la función del flujo
     next();
-
 };
-
 
 module.exports = { validarJWT };
